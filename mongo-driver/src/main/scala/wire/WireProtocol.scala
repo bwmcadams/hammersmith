1e6c0f32eb4514404ad70221d5b79063b2243fdc
Brendan W. McAdams <brendan@10gen.com>
2011-04-03 18:46:25 -0400

    Finished implementing traits and flag shifting for all wire protocol messages

--- mongo-driver/src/main/scala/wire/WireProtocol.scala
+++ mongo-driver/src/main/scala/wire/WireProtocol.scala
@@ -26,7 +26,7 @@ import java.nio.ByteOrder
 import java.util.concurrent.atomic.AtomicInteger
 import org.bson.io.PoolOutputBuffer
 import org.bson.{ BSONEncoder, BSONObject }
-import org.jboss.netty.buffer.{ChannelBufferOutputStream , ChannelBuffers}
+import org.jboss.netty.buffer.{ ChannelBufferOutputStream, ChannelBuffers }
 import org.jboss.netty.channel.Channel
 
 /**
@@ -133,9 +133,9 @@ abstract class MongoMessage extends Logging {
 }
 
 object UpdateFlag extends Enumeration {
-  val Upsert = Value(0)
-  val MultiUpdate = Value(1)
-  // 2-31 are reserved for future usage
+  val Upsert = Value(1 << 0)
+  val MultiUpdate = Value(1 << 1)
+  // Bits 2-31 are reserved for future usage
 }
 
 /**
@@ -152,7 +152,7 @@ trait UpdateMessage extends MongoMessage {
   // val header: MessageHeader // standard message header
   val opCode = OpCode.OpUpdate
 
-  val ZERO: Int // 0 - reserved for future use
+  val ZERO: Int = 0 // 0 - reserved for future use
   val namespace: String // Full collection name (dbname.collectionname)
   def flags: Int = { // bit vector of UpdateFlags assembled from UpdateFlag
     var _f = 0
@@ -161,14 +161,14 @@ trait UpdateMessage extends MongoMessage {
     _f
   }
 
-  val upsert = false
-  val multiUpdate = false
+  val upsert: Boolean
+  val multiUpdate: Boolean
 
   val query: BSONDocument // The query document to select from mongo
   val update: BSONDocument // The document specifying the update to perform
 
-  protected def writeMessage(enc: BSONEncoder) = {
-    enc.writeInt(0)
+  protected def writeMessage(enc: BSONEncoder) {
+    enc.writeInt(ZERO)
     enc.writeCString(namespace)
     enc.writeInt(flags)
     // TODO - Check against Max BSON Size
@@ -190,9 +190,18 @@ trait UpdateMessage extends MongoMessage {
 trait InsertMessage extends MongoMessage {
   //val header: MessageHeader // Standard message header
   val opCode = OpCode.OpInsert
-  val ZERO: Int // 0 - reserved for future use
+
+  val ZERO: Int = 0 // 0 - reserved for future use
   val namespace: String // Full collection name (dbname.collectionname)
   val documents: Seq[BSONDocument] // One or more documents to insert into the collection
+
+  protected def writeMessage(enc: BSONEncoder) {
+    enc.writeInt(ZERO)
+    enc.writeCString(namespace)
+    // TODO - Limit batch insert size which should be 4 * MaxBSON
+    for (doc <- documents) enc.putObject(doc)
+
+  }
 }
 
 object QueryFlag extends Enumeration {
@@ -207,28 +216,28 @@ object QueryFlag extends Enumeration {
    * (CursorNotFound) such as if the final object it points at was
    * deleted.
    */
-  val TailableCursor = Value(1)
+  val TailableCursor = Value(1 << 1)
   /**
    * Allow query from replica slaves.
    */
-  val SlaveOk = Value(2)
+  val SlaveOk = Value(1 << 2)
   /**
    * Used internally for replication, driver should not use.
    */
-  val OpLogReplay = Value(3)
+  val OpLogReplay = Value(1 << 3)
   /**
    * Normally, the mongo server times out an idle cursor after an
    * inactivity period (~10 minutes) to prevent excess memory usage.
    * Setting this option prevents the cursor from timing out.
    */
-  val NoCursorTimeout = Value(4)
+  val NoCursorTimeout = Value(1 << 4)
   /**
    * Used in conjunction with TailableCursor.  If at the end of data,
    * AwaitData blocks rather than returning no data.
    * After a timeout period, returns as normal.
    * In an Async driver we may want to use this with some care.
    */
-  val AwaitData = Value(5)
+  val AwaitData = Value(1 << 5)
   /**
    * Stream the data down "full blast" across multiple "more" packages.
    * This assumes the client will fully read all data queried.
@@ -239,14 +248,14 @@ object QueryFlag extends Enumeration {
    * NOTE: The client is *not* allowed to read all the data unless
    * it closes the connection.
    */
-  val Exhaust = Value(6)
+  val Exhaust = Value(1 << 6)
   /**
    * Get partial results from a mongos if some shards are down,
    * instead of the default behavior of throwing an error.
    */
-  val Partial = Value(7)
+  val Partial = Value(1 << 7)
 
-  // 8-31 are reserved for future usage
+  // Bits 8-31 are reserved for future usage
 }
 /**
  * OP_QUERY Message
@@ -260,12 +269,40 @@ object QueryFlag extends Enumeration {
 trait QueryMessage extends MongoMessage {
   // val header: MessageHeader // Standard message header
   val opCode = OpCode.OpQuery
-  val flags: Int // bit vector of query options, assembled from QueryFlag
+
+  def flags: Int = { // bit vector of query options, assembled from QueryFlag
+    var _f = 0
+    if (tailableCursor) _f |= QueryFlag.TailableCursor.id
+    if (slaveOk) _f |= QueryFlag.SlaveOk.id
+    if (noCursorTimeout) _f |= QueryFlag.NoCursorTimeout.id
+    if (awaitData) _f |= QueryFlag.AwaitData.id
+    if (exhaust) _f |= QueryFlag.Exhaust.id
+    if (partial) _f |= QueryFlag.Partial.id
+    _f
+  }
+
+  val tailableCursor: Boolean
+  val slaveOk: Boolean
+  val noCursorTimeout: Boolean
+  val awaitData: Boolean
+  val exhaust: Boolean
+  val partial: Boolean
+
   val namespace: String // Full collection name (dbname.collectionname)
   val numberToSkip: Int // number of documents to skip
   val numberToReturn: Int // number of docs to return in first OP_REPLY batch
   val query: BSONDocument // BSON Document representing the query
   val returnFields: Option[BSONDocument] = None // Optional BSON Document for fields to return
+
+  protected def writeMessage(enc: BSONEncoder) {
+    enc.writeInt(flags)
+    enc.writeCString(namespace)
+    enc.writeInt(numberToSkip)
+    enc.writeInt(numberToReturn)
+    enc.putObject(query)
+    // TDOO - Not sure what to write for None as this is optional
+    enc.putObject(returnFields.getOrElse(null))
+  }
 }
 
 /**
@@ -280,10 +317,17 @@ trait QueryMessage extends MongoMessage {
 trait GetMoreMessage extends MongoMessage {
   //val header: MessageHeader // Standard message header
   val opCode = OpCode.OpGetMore
-  val ZERO: Int // 0 - reserved for future use
+  val ZERO: Int = 0 // 0 - reserved for future use
   val namespace: String // Full collection name (dbname.collectionname)
   val numberToReturn: Int // number of docs to return in first OP_REPLY batch
   val cursorID: Long // CursorID from the OP_REPLY (DB Genned value)
+
+  protected def writeMessage(enc: BSONEncoder) {
+    enc.writeInt(ZERO)
+    enc.writeCString(namespace)
+    enc.writeInt(numberToReturn)
+    enc.writeLong(cursorID)
+  }
 }
 
 object DeleteFlag extends Enumeration {
@@ -291,9 +335,9 @@ object DeleteFlag extends Enumeration {
    * If set, remove only first matching doc.
    * Default behavior removes all matching documents.
    */
-  val SingleRemove = 0
+  val SingleRemove = Value(1 << 0)
 
-  // 1-31 are reserved for future usage
+  // Bits 1-31 are reserved for future usage
 }
 
 /**
@@ -309,10 +353,24 @@ object DeleteFlag extends Enumeration {
 trait DeleteMessage extends MongoMessage {
   // val header: MessageHeader // Standard message header
   val opCode = OpCode.OpDelete
-  val ZERO: Int // 0 - reserved for future use
+  val ZERO: Int = 0 // 0 - reserved for future use
   val namespace: String // Full collection name (dbname.collectionname)
-  val flags: Int // bit vector of delete flags assembled from DeleteFlag
+  def flags: Int = { // bit vector of delete flags assembled from DeleteFlag
+    var _f = 0
+    if (removeSingle) _f |= DeleteFlag.SingleRemove.id
+    _f
+  }
+
+  val removeSingle: Boolean // Remove only first matching document
+
   val query: BSONDocument // Query object for what to delete
+
+  protected def writeMessage(enc: BSONEncoder) {
+    enc.writeInt(ZERO)
+    enc.writeCString(namespace)
+    enc.writeInt(flags)
+    enc.putObject(query)
+  }
 }
 
 /**
@@ -327,9 +385,15 @@ trait DeleteMessage extends MongoMessage {
 trait KillCursorsMessage extends MongoMessage {
   // val header: MessageHeader // Standard message header
   val opCode = OpCode.OpKillCursors
-  val ZERO: Int // 0 - reserved for future use
+  val ZERO: Int = 0 // 0 - reserved for future use
   val numCursors: Int // The number of cursorIDs in the message
   val cursorIDs: Seq[Long] // Sequence of cursorIDs to close
+
+  protected def writeMessage(enc: BSONEncoder) {
+    enc.writeInt(ZERO)
+    enc.writeInt(numCursors)
+    for (_id <- cursorIDs) enc.writeLong(_id)
+  }
 }
 
 /** OP_MSG Deprecated and not implemented */
@@ -339,19 +403,19 @@ object ReplyFlag extends Enumeration {
    * Set when getMore is called but the cursorID is not valid
    * on the server.  Returns with 0 results.
    */
-  val CursorNotFound = Value(0)
+  val CursorNotFound = Value(1 << 0)
 
   /**
    * Set when a query fails.  Results consist of one document
    * containing an "$err" field describing the failure.
    */
-  val QueryFailure = Value(1)
+  val QueryFailure = Value(1 << 1)
 
   /**
    * Drivers should ignore this; only mongos ever sees it set,
    * in which case it needs to update config from the server
    */
-  val ShardConfigState = Value(2)
+  val ShardConfigState = Value(1 << 2)
 
   /**
    * Set when the server supports the AwaitData Query Option.  If
@@ -359,9 +423,9 @@ object ReplyFlag extends Enumeration {
    * a Tailable cursor.  MongoDB 1.6+ support AwaitData and thus always
    * sets AwaitCapable
    */
-  val AwaitCapable = Value(3)
+  val AwaitCapable = Value(1 << 3)
 
-  // 4-31 are reserved for future usage
+  // Bits 4-31 are reserved for future usage
 }
 /**
  * OP_REPLY
@@ -373,9 +437,17 @@ object ReplyFlag extends Enumeration {
 trait ReplyMessage extends MongoMessage {
   //val header: MessageHeader // Standard message header
   val opCode = OpCode.OpReply
+  // TODO - Read flags in!
   val flags: Int // bit vector of reply flags, available in ReplyFlag
+  def cursorNotFound = flags & ReplyFlag.CursorNotFound.id
+  def queryFailure = flags & ReplyFlag.QueryFailure.id
+  def awaitCapable = flags & ReplyFlag.AwaitCapable.id
   val cursorID: Long // cursorID, for client to do getMores
   val startingFrom: Int // Where in the cursor this reply starts at
   val numReturned: Int // Number of documents in the reply.
   val documents: Seq[BSONDocument] // Sequence of documents
+
+  protected def writeMessage(enc: BSONEncoder) =
+    throw new UnsupportedOperationException("This message is not capable of being written. "
+      + "Replies come only from the server.")
 }
