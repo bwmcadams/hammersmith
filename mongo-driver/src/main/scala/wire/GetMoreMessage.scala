ad9c2377208af39682925ab26959db66e5f67bd1
Brendan McAdams <brendan@bytes.codes>
2011-04-04 12:19:34 -0400

    Separated Messages into individual files for easier management, they grew beyond what I initially expected

---
+++ mongo-driver/src/main/scala/wire/GetMoreMessage.scala
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2010, 2011 10gen, Inc. <http://10gen.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package com.mongodb
+package wire
+
+import org.bson.BSONEncoder
+
+/**
+ * OP_GET_MORE Message
+ *
+ * OP_GET_MORE is used to query the database for documents in a collection.
+ *
+ * The database will respond to OP_GET_MORE messages with OP_REPLY.
+ *
+ * @see http://www.mongodb.org/display/DOCS/Mongo+Wire+Protocol#MongoWireProtocol-OPGETMORE
+ */
+trait GetMoreMessage extends MongoMessage {
+  //val header: MessageHeader // Standard message header
+  val opCode = OpCode.OpGetMore
+  val ZERO: Int = 0 // 0 - reserved for future use
+  val namespace: String // Full collection name (dbname.collectionname)
+  val numberToReturn: Int // number of docs to return in first OP_REPLY batch
+  val cursorID: Long // CursorID from the OP_REPLY (DB Genned value)
+
+  protected def writeMessage(enc: BSONEncoder) {
+    enc.writeInt(ZERO)
+    enc.writeCString(namespace)
+    enc.writeInt(numberToReturn)
+    enc.writeLong(cursorID)
+  }
+}
---
+++ mongo-driver/src/main/scala/wire/UpdateMessage.scala
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2010, 2011 10gen, Inc. <http://10gen.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.mongodb
+package wire
+
+import org.bson.BSONEncoder
+
+/**
+ * OP_UPDATE Message
+ *
+ * OP_UPDATE is used to update a document in a given collection.
+ *
+ * There is no server response to OP_UPDATE
+ * you must use getLastError()
+ *
+ * @see http://www.mongodb.org/display/DOCS/Mongo+Wire+Protocol#MongoWireProtocol-OPUPDATE
+ */
+trait UpdateMessage extends MongoMessage {
+  // val header: MessageHeader // standard message header
+  val opCode = OpCode.OpUpdate
+
+  val ZERO: Int = 0 // 0 - reserved for future use
+  val namespace: String // Full collection name (dbname.collectionname)
+  def flags: Int = { // bit vector of UpdateFlags assembled from UpdateFlag
+    var _f = 0
+    if (upsert) _f |= UpdateFlag.Upsert.id
+    if (multiUpdate) _f |= UpdateFlag.MultiUpdate.id
+    _f
+  }
+
+  val upsert: Boolean
+  val multiUpdate: Boolean
+
+  val query: BSONDocument // The query document to select from mongo
+  val update: BSONDocument // The document specifying the update to perform
+
+  protected def writeMessage(enc: BSONEncoder) {
+    enc.writeInt(ZERO)
+    enc.writeCString(namespace)
+    enc.writeInt(flags)
+    // TODO - Check against Max BSON Size
+    enc.putObject(query)
+    enc.putObject(update)
+  }
+}
+
+object UpdateFlag extends Enumeration {
+  val Upsert = Value(1 << 0)
+  val MultiUpdate = Value(1 << 1)
+  // Bits 2-31 are reserved for future usage
+}
