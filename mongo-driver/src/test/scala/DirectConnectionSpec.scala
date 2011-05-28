7523be7163b24b8d4e752a83622f8c72d301f627
Brendan McAdams <brendan@bytes.codes>
2011-05-28 00:08:29 -0400

    Migrated Specs to acceptance spec

--- mongo-driver/src/test/scala/DirectConnectionSpec.scala
+++ mongo-driver/src/test/scala/DirectConnectionSpec.scala
@@ -18,113 +18,151 @@ package com.mongodb.async
 package test
 
 import com.mongodb.async._
+import futures.RequestFutures
 import org.bson.collection.Document
-import org.specs2.mutable._
 import org.specs2.time.Time._
 import org.bson.util.Logging
+import org.bson.types._
+import org.specs2.execute.Result
+import org.specs2.Specification
+import org.specs2.specification._
 
+class DirectConnectionSpec extends Specification with Logging { def is =
+  "The MongoDB Direct Connection"                          ^
+    "Connect correctly and grab isMaster, then disconnect" ! mongo(connectIsMaster)^
+    "Iterate a simple cursor correctly"                    ! mongo(iterateSimpleCursor)^
+    "Iterate a complex (iteratee) cursor correctly"        ! mongo(iterateComplexCursor)^
+    "Correctly calculate values for 'distinct'"            ! mongo(distinctValue)^
+                                                           endp^
+  "The write behavior"                                     ^
+    "Support 'blind' (NoOp) writes"                        ! mongo(noopInsert)^
+    "Support inserts with no (default) write concern"      ! mongo(insertWithDefaultWriteConcern)^
+    "Support inserts with implicit safe write concern"     ! mongo(insertWithSafeImplicitWriteConcern)^
+                                                           end
+    
 
-class DirectConnectionSpec extends Specification with Logging {
-  //  println(org.apache.commons.logging.Log)
+  trait mongoConn extends AroundOutside[MongoConnection] {
 
-  "The MongoDB Direct Connection" should {
-    val conn = MongoConnection("localhost")
-    "Connect correctly and grab isMaster" in {
+    var conn = MongoConnection()
 
-
-      conn.databaseNames({ dbs: Seq[String] => dbs.foreach(log.trace("DB: %s", _)) })
-
-      conn("test").collectionNames({ colls: Seq[String] => colls.foreach(log.trace("Collection: %s", _)) })
-
-
+    def around[T <% Result](t: =>T) = {
       conn.connected_? must eventually(beTrue)
-
+      t
+      /*conn.close()
+      conn.connected_? must eventually(beFalse)*/
     }
-    // todo - this relies heavily on whats on my local workstation; needs to be generic
-    "Iterate a Cursor Correctly" in {
-      var x = 0
-      conn("bookstore").find("inventory")(Document.empty, Document.empty)((cursor: Cursor) => {
-        log.debug("Got a result from 'find' command")
-        for (doc <- cursor) {
+
+    def outside: MongoConnection = { conn = MongoConnection()
+                                     conn }
+  }
+
+  object mongo extends mongoConn
+
+  def connectIsMaster(conn: MongoConnection) = {
+    conn.databaseNames({ dbs: Seq[String] => dbs.foreach(log.trace("DB: %s", _)) })
+
+    conn("test").collectionNames({ colls: Seq[String] => colls.foreach(log.trace("Collection: %s", _)) })
+
+    conn.connected_? must eventually(beTrue)
+
+  }
+
+  // todo - this relies heavily on whats on my local workstation; needs to be generic
+  def iterateSimpleCursor(conn: MongoConnection) = {
+    var x = 0
+    conn("bookstore").find("inventory")(Document.empty, Document.empty)((cursor: Cursor) => {
+      for (doc <- cursor) {
+        x += 1
+      }
+    })
+
+    x must eventually (be_==(336))
+  }
+
+  def iterateComplexCursor(conn: MongoConnection) = {
+    var x = 0
+    conn("bookstore").find("inventory")(Document.empty, Document.empty)((cursor: Cursor) => {
+      def next(op: Cursor.IterState): Cursor.IterCmd = op match {
+        case Cursor.Entry(doc) => {
           x += 1
-          log.trace("Got a doc: %s", x)
+          if (x < 100) Cursor.Next(next) else Cursor.Done
         }
-        log.info("Done iterating, %s results.", x)
-      })
-
-      x must eventually (be_==(336))
-    }
-    "Iterate a Cursor Correctly" in {
-      var x = 0
-      conn("bookstore").find("inventory")(Document.empty, Document.empty)((cursor: Cursor) => {
-        log.debug("Got a result from 'find' command")
-        log.info("Done iterating, %s results.", x)
-        def next(op: Cursor.IterState): Cursor.IterCmd = op match {
-          case Cursor.Entry(doc) => {
-            x += 1
-            if (x < 100) Cursor.Next(next) else Cursor.Done
-          }
-          case Cursor.Empty => {
-            log.trace("Empty... Next batch.")
-            if (x < 100) Cursor.NextBatch(next) else Cursor.Done
-          }
-          case Cursor.EOF => {
-            log.info("EOF... Cursor done.")
-            Cursor.Done
-          }
+        case Cursor.Empty => {
+          if (x < 100) Cursor.NextBatch(next) else Cursor.Done
         }
-        Cursor.iterate(cursor)(next)
-      })
-
-      x must eventually(5, 5.seconds) (be_==(100))
-    }
-    "Support Distinct" in {
-      conn("bookstore")("inventory").distinct("author")((values: Seq[Any]) => {
-        for (item <- values) {
-          log.trace("Got a value: %s", item.asInstanceOf[String])
+        case Cursor.EOF => {
+          Cursor.Done
         }
-      })
+      }
+      Cursor.iterate(cursor)(next)
+    })
 
-      conn must not beNull
-    }
+    x must eventually(5, 5.seconds) (be_==(100))
+  }
 
-    "Support blind (no check/callback) inserts" in {
-      val mongo = conn("testHammersmith")("test_insert")
-      mongo.dropCollection()(success => {
-        log.info("Dropped collection... Success? " + success)
-      })
-      mongo.insert(Document("foo" -> "bar", "bar" -> "baz")){}
-      success
-    }
-    "Support inserts with no (default) write concern" in {
-      val mongo = conn("testHammersmith")("test_insert")
-      mongo.dropCollection()(success => {
-        log.info("Dropped collection... Success? " + success)
-      })
-      var id: AnyRef = null
-      mongo.insert(Document("foo" -> "bar", "bar" -> "baz"))((oid: Option[AnyRef], res: WriteResult) => {
-        log.info("Result ID: %s Res: %s", oid, res)
-        id = oid.getOrElse(null)
-      })
-      id must not beNull
-    }
-    "Support inserts with implicit safe write concern" in {
-      val mongo = conn("testHammersmith")("test_insert")
-      implicit val safeWrite = WriteConcern.Safe
-      mongo.dropCollection()(success => {
-        log.info("Dropped collection... Success? " + success)
-      })
-      var id: AnyRef = null
-      mongo.insert(Document("foo" -> "bar", "bar" -> "baz"))((oid: Option[AnyRef], res: WriteResult) => {
-        log.info("Result ID: %s Res: %s", oid, res)
-        id = oid.getOrElse(null)
-      })
-      id must not beNull
-    }
+  def distinctValue(conn: MongoConnection) = {
+    conn("bookstore")("inventory").distinct("author")((values: Seq[Any]) => {
+      for (item <- values) {
+        log.trace("Got a value: %s", item.asInstanceOf[String])
+      }
+    })
+
+    success
+  }
+
+  def noopInsert(conn: MongoConnection) = {
+    val mongo = conn("testHammersmith")("test_insert")
+    mongo.dropCollection()(success => {
+      log.info("Dropped collection... Success? " + success)
+    })
+    mongo.insert(Document("foo" -> "bar", "bar" -> "baz")){}
+    success
+  }
+
+  def insertWithDefaultWriteConcern(conn: MongoConnection) = {
+    val mongo = conn("testHammersmith")("test_insert")
+    mongo.dropCollection()(success => {
+      log.info("Dropped collection... Success? " + success)
+    })
+    var id: AnyRef = null
+    mongo.insert(Document("foo" -> "bar", "bar" -> "baz"))((oid: Option[AnyRef], res: WriteResult) => {
+      id = oid.getOrElse(null)
+    })
+    success
+  }
+
+  def insertWithSafeImplicitWriteConcern(conn: MongoConnection) = {
+    val mongo = conn("testHammersmith")("test_insert")
+    implicit val safeWrite = WriteConcern.Safe
+    mongo.dropCollection()(success => {
+      log.info("Dropped collection... Success? " + success)
+    })
+    var id: Option[AnyRef] = null
+    var ok: Option[Boolean] = None
+
+    val handler = RequestFutures.write((result: Either[Throwable, (Option[AnyRef], WriteResult)]) => { 
+      log.info("HANDLER INVOKED. '%s'", result)
+      result match {
+        case Right((oid, wr)) => {
+          ok = Some(true)
+          log.info("Result ID: %s Res: %s", oid, wr)
+          id = oid        }
+        case Left(t) => {
+          ok = Some(false)
+          log.error(t, "Command Failed.")
+        }
+      }
+    })
+    mongo.insert(Document("foo" -> "bar", "bar" -> "baz"))(handler)
+    ok must eventually { beSome(true) }
+    //id must eventually(beSome[ObjectId].asInstanceOf[org.specs2.matcher.Matcher[AnyRef]])
+    log.info("Post insert... %s", id)
+    id must not (beNull.eventually)
+    
+  }
 /*    "Support findAndModify" in {
       val mongo = conn("testHammersmith")("test_findModify")
       mongo.insert(Document("x" -> 1), Document("x" -> 2), Document("x" -> 3))
 
     }*/
-  }
 }
