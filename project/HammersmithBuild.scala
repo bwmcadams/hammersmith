f6d77ee0c1e2fd39dc7d0381169f3b2a03a2d17e
Brendan W. McAdams <brendan@10gen.com>
2011-11-08 22:16:39 +0100

    Build of Hammersmith 0.2.9 - Updated to Java Driver 2.7.1 - Crossbuild for 2.9.0-1 and 2.9.1 - Updated Netty Dep - Updated Twitter Util Dep

--- project/HammersmithBuild.scala
+++ project/HammersmithBuild.scala
@@ -1,5 +1,6 @@
 import sbt._
 import Keys._
+import com.typesafe.sbtscalariform.ScalariformPlugin
 import ScalariformPlugin.{ format, formatPreferences }
 
 object HammersmithBuild extends Build {
@@ -8,8 +9,9 @@ object HammersmithBuild extends Build {
 
   lazy val buildSettings = Seq(
     organization := "com.mongodb.async",
-    version := "0.2.8-SNAPSHOT",
-    scalaVersion := "2.9.0-1"
+    version := "0.2.9",
+    scalaVersion := "2.9.1",
+    crossScalaVersions := Seq("2.9.1", "2.9.0-1")
   )
 
   /**
@@ -41,8 +43,8 @@ object HammersmithBuild extends Build {
 
 
   lazy val defaultSettings = baseSettings ++ Seq(
-    libraryDependencies ++= Seq(casbah, commonsPool, scalaj_collection, netty, twitterUtilCore, slf4j, specs2),
-    resolvers ++= Seq(jbossRepo, sbtReleases, sbtSnapshots, twttrRepo),
+    libraryDependencies ++= Seq(commonsPool, scalaj_collection, netty, twitterUtilCore, slf4j, specs2),
+    resolvers ++= Seq(sonaReleases, jbossRepo, sbtReleases, sbtSnapshots, twttrRepo),
     autoCompilerPlugins := true,
     parallelExecution in Test := true,
     testFrameworks += TestFrameworks.Specs2
@@ -59,7 +61,7 @@ object HammersmithBuild extends Build {
     id = "bson-driver",
     base = file("bson-driver"),
     settings = defaultSettings ++ Seq(
-      libraryDependencies += mongoJava
+      libraryDependencies ++= Seq(bsonJava, mongoJava)
     )
   )
 
@@ -89,28 +91,44 @@ object Publish {
 }
 
 object Dependencies {
-  val casbah = "com.mongodb.casbah" %% "casbah-util" % "2.2.0-SNAPSHOT"
+  //BSON 
+  val bsonJava = "org.mongodb" % "bson" % "2.7.1" 
   // Connection Pooling
   val commonsPool = "commons-pool" % "commons-pool" % "1.5.5"
 
-  val scalaj_collection = "org.scalaj" %% "scalaj-collection" % "1.1"
+  val scalaj_collection = "org.scalaj" %% "scalaj-collection" % "1.2"
   // Netty
-  val netty = "org.jboss.netty" % "netty" % "3.2.4.Final"
+  val netty = "org.jboss.netty" % "netty" % "3.2.6.Final"
 
   // Twitter-util
-  val twitterUtilCore = "com.twitter" % "util-core" % "1.8.15"
+  val twitterUtilCore = "com.twitter" % "util-core" % "1.12.2"
 
   // Testing Deps
-  val specs2 = "org.specs2" %% "specs2" % "1.4" % "test"
-  val mongoJava = "org.mongodb" % "mongo-java-driver" % "2.6.3" % "test->default"
+  val specs2 = "org.specs2" %% "specs2" % "1.5" % "provided" 
+  val mongoJava = "org.mongodb" % "mongo-java-driver" % "2.7.1" % "test->default"
   val slf4j = "org.slf4j" % "slf4j-api" % "1.6.1"
   val slf4jJCL = "org.slf4j" % "slf4j-jcl" % "1.6.1" % "test"
+
+  def specs2ScalazCore(scalaVer: sbt.SettingKey[String]) = 
+    scalaVersionString(scalaVer) match {
+      case "2.8.1" => "org.specs2" %% "specs2-scalaz-core" % "5.1-SNAPSHOT" % "test"
+      case _ => "org.specs2" %% "specs2-scalaz-core" % "6.0.RC2" % "test"
+    }
+
+  def scalaVersionString(scalaVer: sbt.SettingKey[String]): String = {
+    var result = ""
+    scalaVer { sv => result = sv }
+    if (result == "") result = "2.8.1"
+    result
+  }
+
 }
 
 object Resolvers {
   val sbtSnapshots = "snapshots" at "http://scala-tools.org/repo-snapshots"
   val sbtReleases  = "releases" at "http://scala-tools.org/repo-releases"
 
+  val sonaReleases = "releases" at "https://oss.sonatype.org/content/repositories/releases"
   val jbossRepo = "JBoss Public Repo" at "https://repository.jboss.org/nexus/content/groups/public-jboss/"
   val twttrRepo = "Twitter Public Repo" at "http://maven.twttr.com"
 }
