ea68e3aee69486f296b7e02128b92de216747986
Brendan McAdams <brendan@boldradius.com>
2015-10-08 19:44:31 -0400

    Kill the aborted Rx experiment

--- core/src/main/scala/codes/bytes/hammersmith/util/rx/operators/Exists.scala
+++
@@ -1,7 +0,0 @@
-
-package codes.bytes.hammersmith.util.rx
-package operators
-
-object ExistsOperator {
-  def apply[T](source: MongoObservable[T], p: (T) => Boolean) = new GenericRxOperator[T](source, p, false)
-}
